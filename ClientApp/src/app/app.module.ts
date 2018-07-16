7c5493567f0acb2e0788a8391de3d982a6ff4855
QMRaju <QMRaju@hotmail.com>
2018-07-16 13:44:09 -0400

    Done By Raju

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,43 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './User/Registration/registration.component';
 import { LoginComponent } from './User/Login/login.component';
 
 import { LoginService } from './Service/login.service';
-import {HomeComponent} from './Home/home.component';
-import {CreateEmComponent} from './User/CreateEmp/createEm.component'
+import { HomeComponent } from './Home/home.component';
+import { CreateEmComponent } from './User/CreateEmp/createEm.component'
 import { UpdateEmComponent } from './User/UpdateEmp/updateEm.component';
 import { EmployeeService } from './Service/empService';
 
 
 const appRoutes: Routes = [
-{ path: 'login', component: LoginComponent }, 
-{ path: 'signup', component: RegistrationComponent },
-//{ path: 'home', component: HomeComponent },
-{ path: 'createEm', component: CreateEmComponent },
-{ path: 'home',component: HomeComponent,
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: RegistrationComponent },
+  //{ path: 'home', component: HomeComponent },
+  { path: 'createEm', component: CreateEmComponent },
+  {
+    path: 'home', component: HomeComponent,
     children: [
-    {
-      path: 'createEm',
-      component: CreateEmComponent,
-      
-    },
-    {
-      path: 'updateEm',
-      component: UpdateEmComponent,
-      
-    }
-  ]
-  
-}
+      {
+        path: 'createEm',
+        component: CreateEmComponent,
 
+      },
+      {
+        path: 'updateEm',
+        component: UpdateEmComponent,
 
- ];
- 
+      }
+    ]
+
+  }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -59,5 +58,5 @@ const appRoutes: Routes = [
   bootstrap: [AppComponent]
 })
 export class AppModule {
-  
- }
+
+}
