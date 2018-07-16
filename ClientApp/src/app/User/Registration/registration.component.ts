b2948b6344814edba2572221005573623e17700f
QMRaju <QMRaju@hotmail.com>
2018-07-16 13:39:53 -0400

    Raju

--- ClientApp/src/app/User/Registration/registration.component.ts
+++ ClientApp/src/app/User/Registration/registration.component.ts
@@ -1,4 +1,4 @@
-import {Component,OnInit} from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 import { NgForm } from '@angular/forms';
 import { IUser } from '../../Model/user.model';
@@ -15,30 +15,28 @@ import { ICredential } from '../../Model/credential.model';
     selector: 'user-registration',
     templateUrl: './registration.component.html',
     styleUrls: ['./registration.component.css'],
-    providers:[LoginService]
+    providers: [LoginService]
 })
 
 export class RegistrationComponent implements OnInit {
-    
-    user:IUser[];
-    errorMessage: any; 
-  
-    constructor(private _loginService:LoginService, private _router: Router ,private _appComponent: AppComponent) {
-     }
-    
+
+    user: IUser[];
+    errorMessage: any;
+
+    constructor(private _loginService: LoginService, private _router: Router, private _appComponent: AppComponent) {
+    }
     ngOnInit() {
     }
-    Register(userForm:NgForm):void{
-      
-       this._loginService.registerUser(userForm.value)  
-        .subscribe((data) => { 
-           this._appComponent.showHideHome=true;
-           this._appComponent.showHideLogin=false;
-           this._router.navigate(['../home']); 
-           
-           
-        }, error => this.errorMessage = error); 
-        
+    Register(userForm: NgForm): void {
+
+        this._loginService.registerUser(userForm.value)
+            .subscribe((data) => {
+                this._appComponent.showHideHome = true;
+                this._appComponent.showHideLogin = false;
+                this._router.navigate(['../home']);
+
+
+            }, error => this.errorMessage = error);
+
     }
-  }
-  
+}
