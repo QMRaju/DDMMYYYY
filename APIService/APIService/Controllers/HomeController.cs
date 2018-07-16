fce2ded0cae0cf6be3cf62509999be3c3e66c67a
QMRaju <QMRaju@hotmail.com>
2018-07-16 13:14:23 -0400

    New Changes

---
+++ APIService/APIService/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using APIService.Models;
+
+namespace APIService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public void Demo(Test obj)
+        {
+
+
+        }
+
+        public IEnumerable<Employee> Get()
+        {
+            using (DemoEntities entities=new DemoEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
